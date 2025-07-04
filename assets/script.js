4b2d7938b4cf977371deec9fc6e715c0fcc36dfa
Miora Andrian <miora.andri@icloud.com>
2025-07-04 18:47:04 +0300

    fix: note-content class css fix

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,16 @@
+
 document.addEventListener("DOMContentLoaded", function(){
     const ajouterNote = document.getElementById("new-note_btn");
     const container = document.getElementById("container");
 
-    ajouterNote.addEventListener("onClick", function(){
+    ajouterNote.addEventListener("click", function(){
         const noteModal = document.createElement("div");
         noteModal.classList.add("note-modal");
 
         noteModal.innerHTML = `
             <div class = "note-modal-content">
-                <h2>Nouvelle Note</h2>
-                <textarea placeholder="Entrez le contenu de votre note ici..."></textarea>
+                <textarea class="note-title" placeholder="Titre..."></textarea>
+                <textarea class="note-content" placeholder="Entrez le contenu de votre note ici..."></textarea>
                 <div class="modal-actions">
                     <button class="save-note">Entregistrer</button>
                     <button class="close-note">Annuler</button>
@@ -23,5 +24,8 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("Votre note a bien ete enregistree");
                 noteModal.remove();
             });
+            noteModal.querySelector(".close-note").addEventListener("click", function(){
+                noteModal.remove();
+            })
     });
 });
